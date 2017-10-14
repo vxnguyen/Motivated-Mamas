2abb9f775e8bd5541350ce74669b9f18fb4d92d6
Vinh Nguyen <vxnguyen01@gmail.com>
2017-10-14 11:35:17 -0400

    style changes to payment success page

--- page-payment-success.php
+++ page-payment-success.php
@@ -17,18 +17,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main inner payment-success">
 
-			<?php
-			while ( have_posts() ) : the_post();
+			<div class="content">
+				<?php
+				while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			endwhile; // End of the loop.
-			?>
+				endwhile; // End of the loop.
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
