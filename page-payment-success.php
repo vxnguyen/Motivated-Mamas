62274ec211c17200591d679c2e584c4d3469506e
Vinh Nguyen <vxnguyen01@gmail.com>
2017-10-14 11:32:25 -0400

    add classes to payment success page

--- page-payment-success.php
+++ page-payment-success.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main inner payment-success">
 
 			<?php
 			while ( have_posts() ) : the_post();
@@ -29,7 +29,6 @@ get_header(); ?>
 
 			endwhile; // End of the loop.
 			?>
-			Payment Success Page
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
